710ed300bb55e7791ac1798c2ba51905465a6df4
Elena Kostrikina <ek371116@gmail.com>
2026-05-06 11:11:15 +0200

    enabled setinterval for random char

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -13,8 +13,8 @@ const RandomChar = () => {
     useEffect( () => {
         updateChar()
 
-       //const timerId = setInterval(updateChar, 10000);
-       //return () => clearInterval(timerId)
+       const timerId = setInterval(updateChar, 10000);
+       return () => clearInterval(timerId)
         //eslint-disable-next-line
     }, [])
 
